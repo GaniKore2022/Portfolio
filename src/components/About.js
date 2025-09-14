1078bb8df041fce8c21f99c310d4b94d3ba8d7c9
Ganesh Kore <115563354+GaniKore2022@users.noreply.github.com>
2025-09-14 16:23:21 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <section id="about" className="about-section">
+      <h2 className="section-title">About Me</h2>
+      <div className="about-content">
+        <p>
+            I have recently completed my B.Tech in Computer Science and Engineering with a specialization in AIML from SR University. I have a strong foundation in Python and machine learning, and I am currently working on developing my skills in Java and Spring Boot.
+        </p>
+        <p>
+          I completed my intermediate education at Shivani Junior College and my schooling at ORBIT e-Techno School.
+        </p>
+        <p>
+          In my free time, I enjoy playing cricket and Chess.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
